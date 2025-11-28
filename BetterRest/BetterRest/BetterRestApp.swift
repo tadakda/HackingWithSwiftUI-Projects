782813ee50f495d780fb5b3418e72d607cce58bd
Kevin Dao <kevindao2006@gmail.com>
2025-11-27 22:44:01 -0600

    betterrest scaffolding

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by kevin dao on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
