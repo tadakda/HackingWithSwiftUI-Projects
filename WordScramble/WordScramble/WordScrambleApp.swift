af710715ee23418f19ffd0495dbe19f460f4d102
Kevin Dao <kevindao2006@gmail.com>
2025-12-04 19:21:13 -0500

    init

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by kevin dao on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
