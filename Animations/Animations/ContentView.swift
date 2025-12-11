5cf31c83ee26d98c5197ba26069f46f2b23984e8
Kevin Dao <kevindao2006@gmail.com>
2025-12-10 22:53:27 -0500

    basic animatinos

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(Color.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 0))
     }
 }
 
