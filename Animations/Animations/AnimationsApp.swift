6dc8b5cf6a7d6c29e6198b6b137a4c2a06df2c70
Kevin Dao <kevindao2006@gmail.com>
2025-12-10 21:54:57 -0500

    init

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by kevin dao on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
